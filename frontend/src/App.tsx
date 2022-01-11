58105606c66264e7bbe2c407b34aed8763e8f643
Flavio Wu <flaviowu@gmail.com>
2022-01-10 22:47:22 -0300

    Update Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,20 @@
-import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Listing from "pages/Listing";
 import Form from "pages/Form";
 
 function App() {
   return (
-  <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Listing/>} />
-      <Route path="form/:movieId" element={<Form/>} />
-    </Routes>
-  </BrowserRouter>);
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
