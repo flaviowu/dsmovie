94d63aad7df44270bcac5e5ef22f491d2ed857eb
Flavio Wu <flaviowu@gmail.com>
2022-01-10 22:39:41 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listing from "pages/Listing";
+import Form from "pages/Form";
 
 function App() {
-  return <Navbar />;
+  return (
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Listing/>} />
+      <Route path="form/:movieId" element={<Form/>} />
+    </Routes>
+  </BrowserRouter>);
 }
 
 export default App;
