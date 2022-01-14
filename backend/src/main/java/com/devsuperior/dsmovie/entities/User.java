e21ad3e3f7864df042477ae8a1f5feb0bbb820d5
Flavio Wu <flaviowu@gmail.com>
2022-01-13 23:57:33 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -14,6 +14,10 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String email;
+	
+	public User() {
+		
+	}
 
 	public User(Long id, String email) {
 		this.id = id;
